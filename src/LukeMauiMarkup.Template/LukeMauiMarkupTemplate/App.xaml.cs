22cc8454fbbdeaddcadba2ccd368e5babc687813
lukewire <lukewire129@gmail.com>
2024-08-01 15:06:51 +0900

    Template 작업 중

---
+++ src/LukeMauiMarkup.Template/LukeMauiMarkupTemplate/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using LukeMauiMarkupTemplate.Pages;
+
+namespace LukeMauiMarkupTemplate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent ();
+
+            MainPage = new MainPage ();
+        }
+    }
+}
---
+++ src/LukeMauiMarkup.Template/LukeMauiMarkupTemplate/Pages/MainPage.cs
@@ -0,0 +1,20 @@
+﻿namespace LukeMauiMarkupTemplate.Pages;
+
+internal class MainPage : LukeContentPage
+{
+    public override void Build()
+        => this.Content (
+                new Grid()
+                    .RowDefinitions(Auto, Star)
+                    .Children(
+                        new Label()
+                            .Text("Hello LukeMauiMarkup"),
+
+                        new Label ()
+                            .Row(1)
+                            .Text ("Hello LukeMauiMarkup")
+                    )
+                .CenterVertical()
+                .CenterHorizontal()
+             );
+}
