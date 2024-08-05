376b0731f861283b06133e3d1e3aadab1113520f
lukewire <lukewire129@gmail.com>
2024-08-05 15:02:36 +0900

    Update .

--- sample/Login/Imports.cs
+++ sample/Login/Imports.cs
@@ -1,10 +1,10 @@
 // MVVM Toolkit
 // .NET MAUI Markup
-global using LukeMauiMarkup;
-global using LukeMauiMarkup.Extentions;
 global using CommunityToolkit.Maui.Markup;
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
+global using LukeMauiMarkup;
+global using LukeMauiMarkup.Extentions;
 // Static
 global using static CommunityToolkit.Maui.Markup.GridRowsColumns;
 global using static Microsoft.Maui.Graphics.Colors;
