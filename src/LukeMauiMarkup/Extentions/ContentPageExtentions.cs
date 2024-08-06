b3ce94d336c14182c39b96d7413e9a88d8fd2454
lukewire <lukewire129@gmail.com>
2024-08-06 16:12:07 +0900

    클래스명, 파일명 바뀜

--- src/LukeMauiMarkup/Extentions/ContentPageExtentions.cs
+++ src/LukeMauiMarkup/Extentions/ContentPageExtentions.cs
@@ -1,21 +1,28 @@
 ﻿namespace LukeMauiMarkup.Extentions;
 
-public static class ContentViewExtentions
+public static class ContentPageExtentions
 {
     public static T Content<T>(this T self,
             Microsoft.Maui.Controls.View content)
-            where T : Microsoft.Maui.Controls.ContentView
+            where T : Microsoft.Maui.Controls.ContentPage
     {
-        self.SetValue (Microsoft.Maui.Controls.ContentView.ContentProperty, content);
+        self.SetValue (Microsoft.Maui.Controls.ContentPage.ContentProperty, content);
         return self;
     }
 
     public static T Content<T>(this T self,
         Func<Microsoft.Maui.Controls.View> configure)
-        where T : Microsoft.Maui.Controls.ContentView
+        where T : Microsoft.Maui.Controls.ContentPage
     {
         var content = configure ();
-        self.SetValue (Microsoft.Maui.Controls.ContentView.ContentProperty, content);
+        self.SetValue (Microsoft.Maui.Controls.ContentPage.ContentProperty, content);
+        return self;
+    }
+    public static T Title<T>(this T self,
+        string title)
+        where T : Microsoft.Maui.Controls.ContentPage
+    {
+        self.SetValue (Microsoft.Maui.Controls.ContentPage.TitleProperty, title);
         return self;
     }
 }
--- src/LukeMauiMarkup/Extentions/ContentViewExtentions.cs
+++ src/LukeMauiMarkup/Extentions/ContentViewExtentions.cs
@@ -1,21 +1,21 @@
 ﻿namespace LukeMauiMarkup.Extentions;
 
-public static class ContentPageExtentions
+public static class ContentViewExtentions
 {
     public static T Content<T>(this T self,
             Microsoft.Maui.Controls.View content)
-            where T : Microsoft.Maui.Controls.ContentPage
+            where T : Microsoft.Maui.Controls.View
     {
-        self.SetValue (Microsoft.Maui.Controls.ContentPage.ContentProperty, content);
+        self.SetValue (Microsoft.Maui.Controls.ContentView.ContentProperty, content);
         return self;
     }
 
     public static T Content<T>(this T self,
         Func<Microsoft.Maui.Controls.View> configure)
-        where T : Microsoft.Maui.Controls.ContentPage
+        where T : Microsoft.Maui.Controls.ContentView
     {
         var content = configure ();
-        self.SetValue (Microsoft.Maui.Controls.ContentPage.ContentProperty, content);
+        self.SetValue (Microsoft.Maui.Controls.ContentView.ContentProperty, content);
         return self;
     }
 }
